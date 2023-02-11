21fe0d1b96c1e80a46a65a96853d4bf858d13a41
lowerednonce <96267699+lowerednonce@users.noreply.github.com>
2023-02-11 23:34:04 +0000

    adding message content and scrolling diplay to webui

--- webui/index.js
+++ webui/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const SERVER_ID = "1037637575239815220";
+let SERVER_ID = "";
 
 async function read_local_JSON(filename) {
     try {
@@ -9,6 +9,16 @@ async function read_local_JSON(filename) {
         return json_data;
     } catch (error) {
         console.error('Error:', error);
+    }
+}
+
+async function read_local_slist() {
+    try {
+        const response = await fetch("export.json");
+        let json_data = await response.json();
+        return json_data;
+    } catch (error) {
+        console.error("[Error] in retrieving server list: ", error);
     }
 }
 
@@ -33,47 +43,177 @@ function extract_avatar_url(user) {
     }
 }
 
-let show_members;
+let show_members = () => {
+    document.getElementById("hide-members").style.display = "flex";
+    document.getElementById("show-members").style.display = "none";
+    document.getElementsByClassName("users")[0].style.display = "flex";
+}
+
 let hide_members = () => {
     document.getElementById("hide-members").style.display = "none";
     document.getElementById("show-members").style.display = "flex";
     document.getElementsByClassName("users")[0].style.display = "none";
 }
 
-read_local_JSON("../" + SERVER_ID +"/core.json")
-    .then(data => {
-        console.log(data);
-        const parse_list = [
-            ["name",         data["name"]],
-            ["id",           data["id"]],
-            ["created_at",   convert_date(data["created-at"])],
-            ["member_count", data["members"].length],
-            ["locale",       data["preferred_locale"]]
-        ];
-        parse_list.map(pair => {
-            document.getElementById(pair[0]).innerHTML += pair[1];
+function gen_cmsg_counter(factor, limit) {
+    let counter = 0;
+    return [(e) => {
+        e.preventDefault();
+        if((counter+factor) > limit){
+            console.error("failed to display more messages");
+            console.info("counter at: ", counter, " factor at: ", factor, " limit at: ", limit);
+            return
+        }else {
+            counter += factor;
+        }
+    },
+        () => {return counter}
+    ];
+}
+
+read_local_slist().then(data => {
+    console.info(data);
+
+    data["servers"].map(elem => {
+        console.log(elem);
+        document.getElementById("sselection-form-list").innerHTML += "<option value=\"" + elem["id"] + "\">" + elem["name"] + "</option>"
+    });
+
+})
+
+function show_server(data) {
+    // displaying server info
+    const parse_list = [
+        ["name",         "", data["name"] + (data["bot"] ? " (bot)" : "")],
+        ["id",           "server id: ",     data["id"]],
+        ["created_at",   "creation date: ", convert_date(data["created-at"])],
+        ["member_count", "member count: ",   data["members"].length],
+        ["locale",       "locale: ",         data["preferred_locale"]]
+    ];
+    parse_list.map(pair => {
+        document.getElementById(pair[0]).innerHTML = pair[1] + pair[2];
+    });
+
+    // loading user info 
+    const users_HTML = data["active-users"]
+        .map( user => { 
+           console.info(user);
+            return "<div class=\"active-member\">"
+                + "<img loading=\"lazy\" class=\"active-member-img\" src=\"" + extract_avatar_url(user) + "\" width=\"64\" height=\"64\">"
+                + "<p class=\"active-member-username\"><b>"+ user["name"] + "#" + user["discriminator"] + "</b></p>"
+                + "<p class=\"active-member-id\"><i>id: " + user["id"] + "</i></p>"
+                + "<p class=\"active-member-created\">registered: " + convert_date(user["created_at"]) + "</p>"
+                + "</div>";
+        })
+        .reduce((total, elem) => {
+            return total + elem;
         });
 
-        const users_HTML = data["active-users"]
-            .map( user => { 
-               console.info(user);
-                return "<div class=\"active-member\">"
-                    + "<img loading=\"lazy\" class=\"active-member-img\" src=\"" + extract_avatar_url(user) + "\" width=\"64\" height=\"64\">"
-                    + "<p class=\"active-member-username\"><b>"+ user["name"] + "#" + user["discriminator"] + "</b></p>"
-                    + "<p class=\"active-member-id\"><i>id: " + user["id"] + "</i></p>"
-                    + "<p class=\"active-member-created\">registered: " + convert_date(user["created_at"]) + "</p>"
+    // loading the users HTML as to not bother with it the first time the button is clicked
+    document.getElementsByClassName("users")[0].innerHTML = users_HTML; 
+}
+
+function gen_channel_selector(data) {
+    // clojures to get the data object into another lexical scope
+    return (e) => {
+
+        e.preventDefault();
+
+        const chn_id = document.getElementById("cselection-form-list").value;
+        console.log("working with channel id: ", chn_id);
+
+        const chn = data["channels"].filter((i) => {
+            return (i["id"] == chn_id)
+        })[0]
+
+        console.log(chn);
+
+        const parse_list = [
+                ["cname", "channel name: ",             chn["name"] + " (" + chn["type"] + ")" ],
+                ["cdesc", "channel desciption: ",       chn["topic"]??"<i>not provided</i>"],
+                ["cnsfw", "NSFW: ",                     chn["nsfw"] ],
+                ["cid",   "id: ",                       "<i>" + chn["id"] + "</i>"],
+                ["ccreated", "created: ",               convert_date(chn["created_at"])],
+                ["cmsg_count", "total message count: ", (chn["messages"]??0).length]
+        ]
+
+        parse_list.map((line) => {
+            document.getElementById(line[0]).innerHTML = line[1] + line[2];
+        });
+
+        if(chn['type'] == "text") {
+
+            const cmsg_counters = gen_cmsg_counter(50,chn["messages"].length);
+            const cmsg_counter_setter = cmsg_counters[0];
+            const cmsg_counter_getter = cmsg_counters[1];
+
+            // console.info((chn["messages"]) == (chn["messages"].sort((e,p) => {return e["created_at"]>["created_at"]})))
+            
+            
+            const render_cmsg = () => {
+                console.log("render called");
+                console.log("splicing from ", cmsg_counter_getter(), " till ", cmsg_counter_getter()+50);
+                document.getElementById("cmsg").innerHTML = chn["messages"].splice(cmsg_counter_getter(),cmsg_counter_getter()+50)
+                .map((e) => {
+                    const cmsg_HTML = "<div class=\"cmsg-div\">"
+                    + "<p class=\"cmsg-content\">" + e["content"].replaceAll("\n", "</br>")
+                    + "</p><p class=\"cmsg-timestamp\">\t(<i>"
+                    + convert_date(e["created_at"]) + "</i>)</p>"
                     + "</div>";
+                    return cmsg_HTML;
+                })
+                .reduce((total, e) => {
+                    return total+e;
+                });
+            };
+
+
+            render_cmsg();
+            // document.getElementById("cmsg").innerHTML += "<button id=\"cmsg-btn\">add more</button>";
+            
+            document.getElementById("cmsg-btn").onclick = (e) => {
+                cmsg_counter_setter(e);
+                render_cmsg();
+            }
+            
+
+        }
+
+    }
+}
+
+const main = () => {
+
+// main function so that the page is fully loaded
+
+document.getElementById("sselection-form-btn").onclick = (e) => {
+
+    e.preventDefault();
+
+    SERVER_ID = document.getElementById("sselection-form-list").value
+    console.log("working with server of id ", SERVER_ID);
+
+    // load server JSON
+    read_local_JSON("../" + SERVER_ID +"/core.json")
+        .then(data => {
+            console.log(data);
+
+            // server info
+            show_server(data);
+            
+            // channel selection
+            const channel_options_HTML = data["channels"].map( chn => {
+                return "<option value=\"" + chn["id"] + "\">" + chn["name"] + "</option>"
             })
             .reduce((total, elem) => {
                 return total + elem;
             });
-        
-        document.getElementsByClassName("users")[0].innerHTML = users_HTML; 
 
-        show_members = () => {
-            document.getElementById("hide-members").style.display = "flex";
-            document.getElementById("show-members").style.display = "none";
-            document.getElementsByClassName("users")[0].style.display = "flex";
-        }
-        
-    });
+            document.getElementById("cselection-form-list").innerHTML = channel_options_HTML;
+
+            document.getElementById("cselection-form-btn").onclick = gen_channel_selector(data);
+
+        });
+}
+
+};
